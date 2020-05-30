27603523f3efa221dac0a46faaf1a0c6477b6bb9
Emanuel Righetto <posta@emanuelrighetto.it>
2020-05-30 13:44:33 +0200

    reformat code

--- queries/init.sql
+++ queries/init.sql
@@ -19,6 +19,7 @@ DROP TABLE IF EXISTS `logs`;
 CREATE TABLE `logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `message` text NOT NULL,
+  `route` text NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -29,45 +30,49 @@ CREATE TABLE `logs` (
 DROP PROCEDURE IF EXISTS acme.publish_message;
 DELIMITER $$
 $$
-CREATE DEFINER=`root`@`%` PROCEDURE `acme`.`publish_message`(IN first_name VARCHAR(255), IN last_name  VARCHAR(255), IN email VARCHAR(255))
+CREATE DEFINER=`root`@`%` PROCEDURE `acme`.`publish_message`( IN routing VARCHAR(255), IN id int, IN first_name VARCHAR(255), IN last_name  VARCHAR(255), IN email VARCHAR(255))
 BEGIN
-   DECLARE cmd TEXT;
-   DECLARE result int(10);
+   SET @AMQP_URL = 'amqp://guest:guest@rabbitmq:5672';
+   SET @AMQP_EXCHANGE = 'contacts.exchange';
+   SET @MESSAGE = json_object('id', id, 'first_name', first_name, 'last_name', last_name, 'email', email);
   
-   SET cmd=CONCAT('bash /opt/pubmess.sh "',
-   TRIM(first_name),
-   '" "',
-   TRIM(last_name),
-   '" "',
-   TRIM(email),
-   '"');
-  
-   INSERT INTO logs (message) VALUES (cmd);
+   INSERT INTO logs (message) VALUES (@MESSAGE);
    
-  SET result = sys_exec(cmd);
+   SET @message_id = (SELECT lib_mysqludf_amqp_sendjson(@AMQP_URL, @AMQP_EXCHANGE, routing, @MESSAGE));
 END$$
 DELIMITER ;
 
 --
--- Trigger structure for table `pubmess_oninsert_trigger`
+-- Trigger structure for `pubmess_oninsert_trigger`
 --
 DROP TRIGGER IF EXISTS acme.pubmess_oninsert_trigger;
 DELIMITER $$
 $$
 CREATE DEFINER=`root`@`%` TRIGGER pubmess_oninsert_trigger  
 AFTER INSERT ON contacts 
-FOR EACH ROW CALL publish_message(NEW.first_name, NEW.last_name, NEW.email)$$
+FOR EACH ROW CALL publish_message('contacts.insert', NEW.id, NEW.first_name, NEW.last_name, NEW.email)$$
 DELIMITER ;
 
 --
--- Trigger structure for table `pubmess_onupdate_trigger`
+-- Trigger structure for `pubmess_onupdate_trigger`
 --
 DROP TRIGGER IF EXISTS acme.pubmess_onupdate_trigger;
 DELIMITER $$
 $$
 CREATE DEFINER=`root`@`%` TRIGGER pubmess_onupdate_trigger  
 AFTER UPDATE ON contacts 
-FOR EACH ROW CALL publish_message(NEW.first_name, NEW.last_name, NEW.email)$$
+FOR EACH ROW CALL publish_message('contacts.update', NEW.id, NEW.first_name, NEW.last_name, NEW.email)$$
+DELIMITER ;
+
+--
+-- Trigger structure for `pubmess_ondelete_trigger`
+--
+DROP TRIGGER IF EXISTS acme.pubmess_ondelete_trigger;
+DELIMITER $$
+$$
+CREATE DEFINER=`root`@`%` TRIGGER `pubmess_ondelete_trigger` 
+AFTER DELETE ON contacts 
+FOR EACH ROW CALL publish_message('contacts.delete', OLD.id, OLD.first_name, OLD.last_name, OLD.email)$$
 DELIMITER ;
 
 --
@@ -75,14 +80,10 @@ DELIMITER ;
 --
 USE mysql;
 
-DROP FUNCTION IF EXISTS lib_mysqludf_sys_info;
-DROP FUNCTION IF EXISTS sys_get;
-DROP FUNCTION IF EXISTS sys_set;
-DROP FUNCTION IF EXISTS sys_exec;
-DROP FUNCTION IF EXISTS sys_eval;
+DROP FUNCTION IF EXISTS lib_mysqludf_amqp_info;
+DROP FUNCTION IF EXISTS lib_mysqludf_amqp_sendjson;
+DROP FUNCTION IF EXISTS lib_mysqludf_amqp_sendstring;
 
-CREATE FUNCTION lib_mysqludf_sys_info RETURNS string SONAME 'lib_mysqludf_sys.so';
-CREATE FUNCTION sys_get RETURNS string SONAME 'lib_mysqludf_sys.so';
-CREATE FUNCTION sys_set RETURNS int SONAME 'lib_mysqludf_sys.so';
-CREATE FUNCTION sys_exec RETURNS int SONAME 'lib_mysqludf_sys.so';
-CREATE FUNCTION sys_eval RETURNS string SONAME 'lib_mysqludf_sys.so';
+CREATE FUNCTION lib_mysqludf_amqp_info RETURNS STRING SONAME 'lib_mysqludf_amqp.so';
+CREATE FUNCTION lib_mysqludf_amqp_sendjson RETURNS STRING SONAME 'lib_mysqludf_amqp.so';
+CREATE FUNCTION lib_mysqludf_amqp_sendstring RETURNS STRING SONAME 'lib_mysqludf_amqp.so';
